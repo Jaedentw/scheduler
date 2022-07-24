ed6f1d1c767ce02d90c40d4d13e91842653a5d33
Jaeden West <jaeden.t.west@gmail.com>
2022-07-24 14:45:25 -0600

    project setup and fixing scripts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import "index.scss";
 
-import Application from "components/Application";
+import Application from "./components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
