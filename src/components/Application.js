e5381fda4141dc399b27b22c6997773aa058321c
Jaedentw <jaeden.t.west@gmail.com>
2022-08-01 14:02:45 -0700

    connects api for daylist

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./compStyles/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from 'axios';
 
 const appointments = {
   "1": {
@@ -61,8 +44,20 @@ const appointments = {
 }; 
 
 export default function Application(props) {
-
+  const [days, setDays] = useState([])
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then((response) => {
+      setDays(response.data)
+    })
+    .catch((error) => {
+      console.log(error.response);
+    })
+  }, [])
+
+  
 
   return (
     <main className="layout">
