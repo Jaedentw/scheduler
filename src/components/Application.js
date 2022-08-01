539876a8bc738b2685c65e448fb0f9a01c5946f1
Jaedentw <jaeden.t.west@gmail.com>
2022-08-01 14:43:26 -0700

    adds helpers directory and it's contents

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,8 +57,6 @@ export default function Application(props) {
     })
   }, [])
 
-  
-
   return (
     <main className="layout">
       <section className="sidebar">
---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function getAppointmentsForDay(state, day) {
+
+  let appointmentsArray = [];
+
+  for (let dayObj of state.days) {
+    if(dayObj.name === day) {
+      for(let id of dayObj.appointments) {
+        if(state.appointments[id]) {
+          appointmentsArray.push(state.appointments[id]);
+        }
+      }
+    }  
+  }
+
+  return appointmentsArray;
+}
